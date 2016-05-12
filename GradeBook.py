a8765754fd5a5f225ccb904ec8b8b3bba95de33d
Darlene Fung <Darlene@Darlenes-MacBook-Pro.local>
2016-05-12 13:54:36 -0400

    commented final program

--- GradeBook.py
+++ GradeBook.py
@@ -1,11 +1,11 @@
-gradeList = [[89, 90, 86, 95, 95, 91],
+gradeList = [[89, 90, 86, 95, 95, 91],		# list of grades
 			 [75, 77, 75, 81, 88, 65], 
 			 [100, 90, 91, 94, 95, 93]]
 
-student1 = sum(gradeList[0])/6
-student2 = sum(gradeList[1])/6
-student3 = sum(gradeList[2])/6
-print("The average of student 1 is: {}".format(sum(gradeList[0])/6))
-print("The average of student 2 is: {}".format(sum(gradeList[1])/6))
-print("The average of student 3 is: {}".format(sum(gradeList[2])/6))
-print("The class average is: {}".format((student1 + student2 + student3)/3))
+student1 = sum(gradeList[0])/6				# average of student 1
+student2 = sum(gradeList[1])/6				# average of student 3
+student3 = sum(gradeList[2])/6				# average of student 3
+print("The average of student 1 is: {}".format(student1) # print statment for student 1
+print("The average of student 2 is: {}".format(student2) # print statment for student 1
+print("The average of student 3 is: {}".format(student3) # print statment for student 1
+print("The class average is: {}".format((student1 + student2 + student3)/3)) # print statment for all students
